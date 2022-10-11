aaff6d8d900e5b96c5c3af92db1431babfad9124
Rhett Muse <rhett.muse123@gmail.com>
2022-10-11 16:32:12 -0500

    define CartItemType, getProducts,

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import { useState } from 'react';
-import { useQuery } from 'react-query/types/react';
-//Components
+import { useQuery } from 'react-query';
+// Components
 import Drawer from '@mui/material';
 import LinearProgress from '@mui/material';
 import Grid from '@mui/material';
 import AddShoppingCartIcon from '@mui/icons-material';
 import Badge from '@mui/material/Badge';
-//Styles
+// Styles
 import { Wrapper } from './App.styles';
+// Types
+export type CartItemType = {
+	id: number;
+	category: string;
+	description: string;
+	image: string;
+	price: number;
+	title: string;
+	amount: number;
+};
+const getProducts = async (): Promise<CartItemType[]> => {
+	return await (await fetch('https://fakestoreapi.com/products')).json();
+};
 
 const App = () => {
+	const { data, isLoading, error } = useQuery<CartItemType[]>(
+		'products',
+		getProducts
+	);
+	console.log(data);
 	return <div className='App'>Start</div>;
 };
 
