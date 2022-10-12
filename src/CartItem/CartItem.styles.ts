3a1902ce98ff7d7a644527176ebe67950f74e0fc
Rhett Muse <rhett.muse123@gmail.com>
2022-10-12 16:59:24 -0500

    define styles

--- src/CartItem/CartItem.styles.ts
+++ src/CartItem/CartItem.styles.ts
@@ -1,3 +1,25 @@
 import styled from 'styled-components';
 
-export const CartItemWrapper = styled.div``;
+export const CartItemWrapper = styled.div`
+	display: flex;
+	justify-content: space-between;
+	font-family: Arial, Helvetica, sans-serif;
+	border-bottom: 1px soild lightblue;
+	padding-bottom: 20px;
+
+	div {
+		flex: 1;
+	}
+
+	.infromation,
+	.buttons {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	img {
+		max-width: 80px;
+		object-fit: contain;
+		magin-left: 40px;
+	}
+`;
