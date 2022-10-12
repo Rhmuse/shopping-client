048e0f155434261247b052034fbad4c0cd3541b9
Rhett Muse <rhett.muse123@gmail.com>
2022-10-12 16:47:00 -0500

    complete CartItem and add to Cart

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -1,9 +1,43 @@
-import Button from '@mui/material';
+import { Button } from '@mui/material';
 // Types
 import { ItemTypeDTO as CartItemType } from '../api/dto/ItemTypeDTO';
 // Styles
 import { CartItemWrapper } from './CartItem.styles';
 
-const CartItem: React.FC = () => <div>Cart Item</div>;
+type Props = {
+	item: CartItemType;
+	addToCart: (clickedItem: CartItemType) => void;
+	removeFromCart: (id: number) => void;
+};
+
+const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
+	<CartItemWrapper>
+		<div>
+			<h3>{item.title}</h3>
+			<div className='information'>
+				<p>Price: ${item.price}</p>
+				<p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+			</div>
+			<div className='buttons'>
+				<Button
+					size='small'
+					disableElevation
+					variant='contained'
+					onClick={() => removeFromCart(item.id)}>
+					-
+				</Button>
+				<p>{item.amount}</p>
+				<Button
+					size='small'
+					disableElevation
+					variant='contained'
+					onClick={() => addToCart(item)}>
+					+
+				</Button>
+			</div>
+		</div>
+		<img src={item.image} alt={item.title} />
+	</CartItemWrapper>
+);
 
 export default CartItem;
