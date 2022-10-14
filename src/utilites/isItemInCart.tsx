00041713d7ffc450df0560e7e28051739c43d586
Rhett Muse <rhett.muse123@gmail.com>
2022-10-14 08:38:57 -0500

    update state management

--- src/utilites/isItemInCart.tsx
+++ src/utilites/isItemInCart.tsx
@@ -1,7 +1,9 @@
 import { ItemTypeDTO as ItemType } from '../api/dto/ItemTypeDTO';
 
+import localState from '../state/localState';
+
 export const isItemInCart = (clickedItem: ItemType): boolean => {
-	const cart: ItemType[] = JSON.parse(window.localStorage.getItem('cart')!);
+	const cart: ItemType[] = localState.cart.getCart();
 
 	if (cart.find((item: ItemType) => item.id === clickedItem.id)) {
 		return true;
