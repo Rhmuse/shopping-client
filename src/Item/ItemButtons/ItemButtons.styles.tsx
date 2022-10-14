645b0cca88b70da4d4c8619c8be72c5a38bfa921
Rhett Muse <rhett.muse123@gmail.com>
2022-10-13 19:15:07 -0500

    add styling to ItemButtons

---
+++ src/Item/ItemButtons/ItemButtons.styles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ItemButtonWrapper = styled.div`
+	.notInCart {
+		display: flex;
+		width: 100%;
+		justify-content: space-between;
+		border-radius: 0 0 20px 20px;
+		flex-direction: column;
+	}
+
+	.inCart {
+	}
+`;
